3c189c84159de84006b79dce34631ecedc331e95
Nastya <a.dementeva@mashtab.org>
2019-04-26 17:27:53 +0300

    FEATURE TG-4110

--- frontend/src/app/main-menu/main-menu.component.ts
+++ frontend/src/app/main-menu/main-menu.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 
 @Component({
   selector: 'vdi-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  styleUrls: ['./main-menu.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity:0}),
+        animate(300, style({opacity:1})) 
+      ]),
+      transition(':leave', [   // :leave is alias to '* => void'
+        animate(0, style({opacity:0})) 
+      ])
+    ]) ]
 })
 
 
-export class MainMenuComponent implements OnInit, OnChanges {
+export class MainMenuComponent implements OnInit {
 
   public listMenu: object[] = [
                                 { name: 'Пулы', icon:'desktop', route:'pools' },
@@ -27,21 +38,8 @@ export class MainMenuComponent implements OnInit, OnChanges {
   ngOnInit() {
   }
 
-  ngOnChanges(a) {
-    console.log(a,'kdkkd');
-  }
-
   public clickItem(index,listMenu) {
-    let i = index;
-    listMenu[i].open = !listMenu[i].open;
-
-    listMenu.forEach((element,index) => {
-      if(index != i) {
-        if(element.open) {
-          element.open = false;
-        }
-      }
-    });
+    listMenu[index].open = !listMenu[index].open;
   }
   
 
