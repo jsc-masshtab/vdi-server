08aa42674dca5963e6b6205b765279beeaee1226
Vitalii Abetkin <v.abetkin@mashtab.org>
2019-05-17 20:39:48 +0300

    backend: restrict client to 1 vm

---
+++ backend/vdi_server/migrations/0017_vm_ADD_user.sql
@@ -0,0 +1,3 @@
+
+alter table vm
+    add column username varchar(100);
--- backend/vdi_server/vdi/app/client_routes.py
+++ backend/vdi_server/vdi/app/client_routes.py
@@ -8,6 +8,8 @@ from starlette.responses import JSONResponse, PlainTextResponse
 from vdi.pool import Pool
 from vdi.tasks import thin_client
 
+from vdi.context_utils import enter_context
+from vdi.db import db
 
 from . import app
 
@@ -15,6 +17,9 @@ from . import app
 @requires('authenticated')
 async def get_pools(request):
     #FIXME filter by user
+    user = request.user.username
+    # query = """
+    # """
     li = [
         {
             'id': id,
@@ -28,12 +33,33 @@ async def get_pools(request):
 @app.route('/client/pools/{pool_id}', methods=['POST'])
 @requires('authenticated')
 async def get_vm(request):
-    pool_id = int(request.path_params['pool_id'])
-    pool = Pool.instances[pool_id]
-    domain = await pool.queue.get()
-    pool.on_vm_taken()
-    addr = await thin_client.PrepareVm(domain_id=domain['id'])
-    return JSONResponse(addr)
+    async with db.connect() as conn:
+        # FIXME
+        controller_ip = '192.168.20.120'
+
+        user = request.user.username
+        pool_id = int(request.path_params['pool_id'])
+        qu = """
+        select id from vm where username = $1 and pool_id = $2
+        """, user, pool_id
+        vms = await conn.fetch(*qu)
+        if vms:
+            [(id,)] = vms
+            info = await thin_client.GetDomainInfo(controller_ip=controller_ip, domain_id=id)
+            return JSONResponse({
+                'host': controller_ip,
+                'port': info['remote_access_port'],
+            })
+        pool = Pool.instances[pool_id]
+        domain = await pool.queue.get()
+        qu = "update vm set username = $1 where id = $2", user, domain['id']
+        await conn.fetch(*qu)
+        pool.on_vm_taken()
+        info = await thin_client.PrepareVm(controller_ip=controller_ip, domain_id=domain['id'])
+        return JSONResponse({
+            'host': controller_ip,
+            'port': info['remote_access_port'],
+        })
 
 
 @app.route('/check', methods=['GET', 'POST'])
