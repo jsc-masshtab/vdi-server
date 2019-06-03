43e103bf145e87b40907238e23408e9b8a80adb0
Vitalii Abetkin <v.abetkin@mashtab.org>
2019-06-03 12:16:36 +0300

    init_vagrant.py

--- init_vagrant.py
+++ init_vagrant.py
@@ -1,32 +1,59 @@
 """
 Usage:
-  init <boxname>
+  init [-i | --interactive] [--bare] [<boxname>]
+
 """
+#TODO clear <boxname>
 
 import docopt
 args = docopt.docopt(__doc__)
 
 import subprocess
 import sys
+import os
+from textwrap import dedent
 
 def main():
-    cmd = f"vagrant init {args['<boxname>']}"
-    completed = subprocess.run(cmd, shell=True)
-    if completed.returncode:
-        sys.exit(completed.returncode)
-    breakpoint()
-    completed = subprocess.run("vagrant up", shell=True)
-    if completed.returncode:
-        sys.exit(completed.returncode)
+    if not args['<boxname>']:
+        boxname = 'generic/ubuntu1904'
+    else:
+        boxname = args['<boxname>']
+    if os.path.exists('Vagrantfile'):
+        ans = input('Delete existing Vagrantfile? [y/n]')
+        if ans.strip().lower() != 'y':
+            sys.exit(0)
+        os.remove('Vagrantfile')
+    if not args['--bare']:
+        # Create a box, update it and commit
+        with open('vagrant/temp.Vagrantfile') as f:
+            content = f.read()
+        if args['-i'] or args['--interactive']:
+            line = dedent(f"""\
+            $boxname = {boxname}
+            $script = <<-SCRIPT SCRIPT
+            """)
+        else:
+            line = dedent(f"""\
+            $boxname = "{boxname}"
+            $script = <<-SCRIPT
+                apt update
+                apt upgrade -y
+            SCRIPT
+            """)
+        vagrantfile = '\n'.join((line, content))
+        with open('Vagrantfile', 'w') as f:
+            f.write(vagrantfile)
+        completed = subprocess.run("vagrant up", shell=True)
+        if completed.returncode:
+            sys.exit(completed.returncode)
+        if args['-i'] or args['--interactive']:
+            completed = subprocess.run("vagrant ssh", shell=True)
+        completed = subprocess.run("vagrant commit", shell=True)
 
-
-    completed = subprocess.run("vagrant ssh", shell=True)
-    completed = subprocess.run("vagrant commit", shell=True)
-    with open('_Vagrantfile') as f:
+    with open('vagrant/real.Vagrantfile') as f:
         content = f.read()
-    boxname = ''.join(('"', args['<boxname>'], '"'))
-    line = f"$boxname = {boxname}"
-    vagrantfile = '\n'.join((line, content))
+    line = f'$boxname = "{boxname}"'
+    vagrantfile = '\n'.join((line, '', content))
     with open('Vagrantfile', 'w') as f:
         f.write(vagrantfile)
 
