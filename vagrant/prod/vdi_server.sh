be84684b07aaf65dac19ee55b58fc64015eeecb3
Solomin <a.solomin@mashtab.org>
2019-09-06 15:25:17 +0300

    Потерянные файлы

---
+++ vagrant/prod/vdi_server.sh
@@ -0,0 +1,16 @@
+cd ../../backend
+
+echo "vdi_server: setting env..."
+echo "export PIPENV_SKIP_LOCK=1" >> /home/vagrant/.bashrc
+export PIPENV_SKIP_LOCK=1
+#export PIPENV_PIPFILE=/vagrant/vagrant/prod/Pipfile
+pipenv install
+
+echo "vdi_server: applying migrations..."
+pipenv run python mi-pkg/mi/main.py
+
+echo "vdi_server: starting server..."
+pkill python
+pipenv run python main.py &
+
+
