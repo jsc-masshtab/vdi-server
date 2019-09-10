f3944e63b34cb00cb8e3219216a5638e948c9635
Nastya <a.dementeva@mashtab.org>
2019-09-10 20:39:42 +0300

    vg

--- vagrant/prod/frontend.sh
+++ vagrant/prod/frontend.sh
@@ -2,6 +2,7 @@ cd ../../frontend
 sudo snap install node --channel 11/stable --classic
 rm -rf node_modules
 /snap/bin/npm install
+/snap/bin/npm rebuild node-sass 
 
 # Somehow doesn't work with nohup... Therefore, should be the last script to execute
 /snap/bin/npm run ng serve -- --host 0.0.0.0
