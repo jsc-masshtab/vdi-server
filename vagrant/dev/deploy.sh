3b26892d45d7b957a0e0f09fb8db3f2cf5cf2bb5
Timofey Karev <t.karev@mashtab.org>
2019-10-18 15:05:19 +0300

    FEATURE TG-5953 vagrant file with deploy script

---
+++ vagrant/dev/deploy.sh
@@ -0,0 +1,41 @@
+echo "Install apt packages"
+
+sudo sed -i s/us\./ru\./g /etc/apt/sources.list
+sudo apt update -y
+sudo apt install --no-install-recommends -y postgresql-server-dev-9.6 python3-dev gcc python3-pip postgresql htop mc # Не нашел на астре пакеты ncdu и bmon
+curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
+sudo apt install -y nodejs
+
+#------------------------------
+echo "Setup database"
+
+sudo sed -i 's/peer/trust/g' /etc/postgresql/9.6/main/pg_hba.conf
+sudo systemctl restart postgresql
+
+echo 'postgres:postgres' | sudo chpasswd
+sudo su postgres -c "psql -c \"ALTER ROLE postgres PASSWORD 'postgres';\" "
+# На астре нету бездуховной кодировки en_US.UTF-8. Есть C.UTF-8
+sudo su postgres -c "psql -c \"create database vdi encoding 'utf8' lc_collate = 'en_US.UTF-8' lc_ctype = 'en_US.UTF-8' template template0;\" "
+
+sudo python3.5 -m pip install pipenv
+
+#------------------------------
+echo "Setup vdi server"
+
+APP_DIR=/opt/veil-vdi
+cd $APP_DIR
+
+echo "vdi_server: setting env..."
+
+export PIPENV_PIPFILE=$APP_DIR/backend/Pipfile_future
+pipenv install
+
+echo "vdi_server: starting server..."
+nohup pipenv run python $APP_DIR/backend/vdi2/app.py & #nohup pipenv run vdi &
+
+#------------------------------
+echo "Setup frontend"
+
+cd $APP_DIR/frontend/
+npm install
+nohup npm run ng serve -- --host 0.0.0.0
