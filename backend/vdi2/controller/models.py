a90c0a48153acb6a6ac6cfcf241600f24d2d4ab7
Aleksey Devyatkin <dal.dev@ya.ru>
2019-10-28 01:21:50 +0300

    FEATURE TG-6074 переезд
    Получение токена авторизации для veil-ecp

--- backend/vdi2/controller/models.py
+++ backend/vdi2/controller/models.py
@@ -3,6 +3,7 @@ import uuid
 from sqlalchemy.dialects.postgresql import UUID
 
 from database import db
+from settings import VEIL_CREDENTIALS
 
 
 class Controller(db.Model):
@@ -14,3 +15,17 @@ class Controller(db.Model):
     description = db.Column(db.Unicode(length=256))
     version = db.Column(db.Unicode(length=128))
     default = db.Column(db.Boolean())
+
+
+class VeilCredentials(db.Model):
+    __tablename__ = 'veil_creds'
+    username = db.Column(db.Unicode(length=128), nullable=False)
+    token = db.Column(db.Unicode(length=1024), nullable=False)
+    controller_ip = db.Column(db.Unicode(length=100), nullable=False)
+    expires_on = db.Column(db.DateTime(timezone=True), nullable=False)
+
+    @staticmethod
+    async def get_token(controller_ip):
+        return await VeilCredentials.select('token').where(
+            (VeilCredentials.username == VEIL_CREDENTIALS['username']) & (
+                    VeilCredentials.controller_ip == controller_ip)).gino.scalar()
