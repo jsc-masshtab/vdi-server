6422ad35ef6d1c76fbf3a5c3d762cda7acf884b1
Solomin <a.solomin@mashtab.org>
2019-11-13 16:51:08 +0300

    FEATURE TG-6079 фикс последовательности миграций

--- backend/vdi2/migrations/versions/c61c8ef17e40_vm_table_username_nullable.py
+++ backend/vdi2/migrations/versions/c61c8ef17e40_vm_table_username_nullable.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c61c8ef17e40'
-down_revision = '80c04faf2107'
+down_revision = '84a595a83c57'
 branch_labels = None
 depends_on = None
 
