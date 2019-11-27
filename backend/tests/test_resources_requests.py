058d2db4873e7e78b99fed85296e32011c4e3055
Solomin <a.solomin@mashtab.org>
2019-11-27 14:35:22 +0300

    FEATURE TG-6383 Тест запроса серверов

--- backend/tests/test_resources_requests.py
+++ backend/tests/test_resources_requests.py
@@ -28,3 +28,48 @@ async def test_request_clusters(fixt_db):
     """
 
     executed = await execute_scheme(resources_schema, qu)
+
+
+@pytest.mark.asyncio
+async def test_request_node(fixt_db):
+
+    qu = """
+    {
+        nodes(ordering: "-verbose_name"){
+        id
+        cpu_count
+        verbose_name
+        cluster{
+            verbose_name
+        }
+        controller {
+            address
+        }
+    }
+    }
+    """
+    executed = await execute_scheme(resources_schema, qu)
+
+    # Чекним запрос определенного сервера (первого в списке), если серверы есть
+    if executed['nodes']:
+        print('__executed', executed)
+        node = executed['nodes'][0]
+
+        qu = """
+        {
+        node(id: "%s", controller_address: "%s"){
+        verbose_name
+        status
+        datacenter {
+            id
+        }
+        cpu_count
+        memory_count
+        management_ip
+        }
+        }
+        """ % (node['id'], node['controller']['address'])
+
+        executed = await execute_scheme(resources_schema, qu)
+        print('___executed', executed)
+        assert node['verbose_name'] == executed['node']['verbose_name']
