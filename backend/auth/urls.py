6bc3f2c4f518450549c421e571f1a2ebe14b9faf
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2019-12-17 18:03:58 +0300

    FEATURE TG-6077 Auth logout method.
    Ендпоинт для выхода из системы - инвалидирует существующий токен пользователя делая невозможным дальнейший вход под ним.

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from auth.handlers import AuthHandler
+from auth.handlers import AuthHandler, LogoutHandler
 
 
 auth_api_urls = [
     (r'/auth/?', AuthHandler),
+    (r'/logout/?', LogoutHandler),
     # (r'/auth/refresh-token/?', RefreshAuthHandler),
 ]
