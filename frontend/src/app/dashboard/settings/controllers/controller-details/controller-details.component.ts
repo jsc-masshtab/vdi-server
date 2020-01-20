190b108c7129d6ab25b9bda83322711c22dc9f16
Nastya <a.dementeva@mashtab.org>
2020-01-20 13:39:09 +0300

    FEATURE TG-6825 контроллер

--- frontend/src/app/dashboard/settings/controllers/controller-details/controller-details.component.ts
+++ frontend/src/app/dashboard/settings/controllers/controller-details/controller-details.component.ts
@@ -1,9 +1,13 @@
+import { RemoveControllerComponent } from './../remove-controller/remove-controller.component';
+import { FormForEditComponent } from './../../../common/forms-dinamic/change-form/form-edit.component';
+import { MatDialog } from '@angular/material';
 import { map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { ControllersService } from './../all-controllers/controllers.service';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IEditFormObj } from 'types';
 
 
 @Component({
@@ -25,12 +29,28 @@ export class ControllerDetailsComponent implements OnInit, OnDestroy {
     {
       title: 'Название',
       property: 'verbose_name',
-      type: 'string'
+      type: 'string',
+      formEdit: [{
+        header: 'Изменение имени контроллера',
+        tag: 'input',
+        type: 'text',
+        fieldName: 'verbose_name',
+        fieldValue: this.controller['verbose_name']
+      }],
+      edit: 'openEditForm',
     },
     {
       title: 'Адрес',
       property: 'address',
-      type: 'string'
+      type: 'string',
+      formEdit: [{
+        header: 'Изменение адреса контроллера',
+        tag: 'input',
+        type: 'text',
+        fieldName: 'address',
+        fieldValue: this.controller['address']
+      }],
+      edit: 'openEditForm',
     },
     {
       title: 'Версия',
@@ -40,23 +60,52 @@ export class ControllerDetailsComponent implements OnInit, OnDestroy {
     {
       title: 'Логин для подключения',
       property: 'username',
-      type: 'string'
+      type: 'string',
+      formEdit: [{
+        header: 'Изменение логина контроллера',
+        tag: 'input',
+        type: 'text',
+        fieldName: 'username',
+        fieldValue: this.controller['username']
+      }],
+      edit: 'openEditForm',
     },
     {
       title: 'Пароль для подключения',
-      property: 'username',
-      type: 'string'
+      property: 'password',
+      type: 'string',
+      formEdit: [{
+        header: 'Изменение пароля контроллера',
+        tag: 'input',
+        type: 'text',
+        fieldName: 'password',
+        fieldValue: this.controller['password']
+      }],
+      edit: 'openEditForm',
     },
     {
       title: 'Использовать ldap подключение',
       property: 'ldap_connection',
-      type: 'string'
-    }
+      type: {
+        typeDepend: 'boolean',
+        propertyDepend: ['Да', 'Нет']
+      },
+      formEdit: [{
+        header: 'Изменение ldap',
+        tag: 'input',
+        type: 'checkbox',
+        fieldName: 'ldap_connection',
+        fieldValue: this.controller['ldap_connection'],
+        description: 'Использовать ldap подключение'
+      }],
+      edit: 'openEditForm',
+    },
   ];
 
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router,
-              private controllerService: ControllersService
+              private controllerService: ControllersService,
+              public  dialog: MatDialog
 ) {}
 
   ngOnInit() {
@@ -99,8 +148,46 @@ export class ControllerDetailsComponent implements OnInit, OnDestroy {
     }
   }
 
-  public removeController() {
+  public edit(line: any) {
+    this[line.edit](line);
+  }
 
+// @ts-ignore: Unreachable code error
+  private openEditForm(activeObj: IEditFormObj): void  {
+    activeObj['formEdit'][0]['fieldValue'] = this.controller[activeObj['property']];
+    this.dialog.open(FormForEditComponent, {
+      width: '500px',
+      data: {
+        post: {
+          service: this.controllerService,
+          method: 'updateController',
+          params: {
+            id: this.idController
+          }
+        },
+        settings: {
+          form: activeObj['formEdit'],
+          buttonAction: 'Изменить',
+          header: activeObj['formEdit'][0]['header']
+        },
+        update: {
+          method: 'getController',
+          params: [
+            this.idController
+          ]
+        }
+      }
+    });
+  }
+
+  public removeController() {
+    this.dialog.open(RemoveControllerComponent, {
+      width: '500px',
+      data: {
+        id: this.idController,
+        verbose_name: this.controller['verbose_name']
+      }
+    });
   }
 
   public close(): void  {
