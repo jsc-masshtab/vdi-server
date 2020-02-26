6c2ff6e97073959b2613f469b7ecf22430572c03
Nastya <a.dementeva@mashtab.org>
2020-02-26 16:25:27 +0300

    FEATURE TG-7263 мэппинги для ад

--- frontend/src/app/dashboard/settings/auth-directory/auth-directory.service.ts
+++ frontend/src/app/dashboard/settings/auth-directory/auth-directory.service.ts
@@ -43,6 +43,7 @@ export class AuthenticationDirectoryService {
                                       verbose_name
                                     }
                                     description
+                                    status
                                 }
                             }
                         }
