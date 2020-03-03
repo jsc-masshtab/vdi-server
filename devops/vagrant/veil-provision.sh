48cb2970e89108a04fddc05c1dcce5d5747b5acf
v.burygin <HeccrfzPtvkz>
2020-03-03 10:41:28 +0300

    FEATURE TG-7322 veil provision part 3

--- devops/vagrant/veil-provision.sh
+++ devops/vagrant/veil-provision.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get install -y sshpass
 
-base_cmd="/var/lib/ecp-veil/cli/app/shell.py api node"
+base_cmd="/var/lib/ecp-veil/cli/app/shell.py api"
 ssh_opts="-q -tt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -oConnectTimeout=5"
 controller_ip="192.168.20.110"
 node1_ip="192.168.20.111"
@@ -30,5 +30,10 @@ for i in $(seq ${attempts_count}); do
 done
 
 echo "run Veil provision"
-sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} list ${api_user} ${api_password} -c ${controller_ip}
-sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} add ${api_user} ${api_password} ${node1_ip} ${node1_ip} ${root_password} -c ${controller_ip}
+# Добавляем узлы
+sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} node add ${api_user} ${api_password} ${node1_ip} ${node1_ip} ${root_password} -c ${controller_ip}
+# sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} node list ${api_user} ${api_password} -c ${controller_ip}
+
+# Добавляем ВМ и шаблон на первом попавшемся активнгом узле
+sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} domain create ${api_user} ${api_password} domain
+sshpass -p ${root_password} ssh ${ssh_opts} root@${controller_ip} ${base_cmd} domain create_template ${api_user} ${api_password} template
