1a36856015e4b5ba6881453e4be58d1d20621e4c
Solomin <a.solomin@mashtab.org>
2020-03-17 17:45:38 +0300

    FEATURE_TG_7582 Pool icon as button

--- desktop-client-c/src/vdi_client/vdi_pool_widget.c
+++ desktop-client-c/src/vdi_client/vdi_pool_widget.c
@@ -15,9 +15,7 @@ VdiPoolWidget build_pool_widget(const gchar *pool_id, const gchar *pool_name,
     vdi_pool_widget.pool_id = g_strdup(pool_id);
 
     //GtkFrame
-    //gchar *header_label = g_strdup_printf("id %s ", pool_id);
-    vdi_pool_widget.main_widget = gtk_frame_new(""); // status
-    //g_free(header_label);
+    vdi_pool_widget.main_widget = gtk_frame_new(NULL); // status
 
     vdi_pool_widget.gtk_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
     // overlay
@@ -45,17 +43,18 @@ VdiPoolWidget build_pool_widget(const gchar *pool_id, const gchar *pool_name,
 
     vdi_pool_widget.image_widget = gtk_image_new_from_resource(os_icon_path);
     free_memory_safely(&os_icon_path);
-    //gtk_box_pack_start((GtkBox *)vdi_pool_widget.gtk_box, vdi_pool_widget.image_widget, TRUE, TRUE, 0);
 
     // vm start button
     vdi_pool_widget.vm_start_button = gtk_button_new_with_label(pool_name);
+
     gtk_button_set_always_show_image(GTK_BUTTON (vdi_pool_widget.vm_start_button), TRUE);
     gtk_button_set_image(GTK_BUTTON (vdi_pool_widget.vm_start_button), vdi_pool_widget.image_widget);
     gtk_button_set_image_position(GTK_BUTTON (vdi_pool_widget.vm_start_button), GTK_POS_BOTTOM);
 
     g_object_set_data((GObject *)vdi_pool_widget.vm_start_button, "pool_id", (gpointer)vdi_pool_widget.pool_id);
     gtk_box_pack_start((GtkBox *)vdi_pool_widget.gtk_box, vdi_pool_widget.vm_start_button, TRUE, TRUE, 0);
-    //
+
+    // main_widget setup
     gtk_widget_set_size_request(vdi_pool_widget.main_widget, 100, 120);
     gtk_flow_box_insert((GtkFlowBox *)gtk_flow_box, vdi_pool_widget.main_widget, 0);
 
