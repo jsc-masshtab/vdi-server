6e5c78b3f9171adda1eb7f3aa381c06f4e0ec084
Ruslan <r.belov@mashtab.org>
2020-04-13 14:52:46 +0300

    FEATURE TG-7985 Дополнительные требования к задачам по лицензированию

--- frontend/src/app/dashboard/common/components/single/footer/footer.service.ts
+++ frontend/src/app/dashboard/common/components/single/footer/footer.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthStorageService } from 'src/app/login/authStorage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FooterService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authStorageService: AuthStorageService) { }
 
   public getInfo(): any {
     return this.http.get('/api/version/');
   }
 
   public getLicence(): any {
-    return this.http.get('/api/license/');
+    let headers = new HttpHeaders().set('Content-Type', 'application/json')
+    .set('Authorization', `jwt ${this.authStorageService.getItemStorage('token')}`)
+    .set('Client-Type', 'angular-web');
+
+    return this.http.get('/api/license/', { headers });
   }
 }
