57d103849376c4010041c6a130b556869413c9e4
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2020-04-16 12:56:43 +0300

    FEATURE TG-8019 repo optimization
    Выселил тонкий клиент в отдельный репозиторий.

--- desktop-client-c/src/rdp_viewer/rdp_data.h
+++
@@ -1,4 +0,0 @@
-#ifndef RDP_DATA_H
-#define RDP_DATA_H
-
-#endif // RDP_DATA_H
--- desktop-client-c/src/utility/crashhandler.h
+++
@@ -1,7 +0,0 @@
-#ifndef CRASHHANDLER_H
-#define CRASHHANDLER_H
-
-
-void install_crash_handler(const char *logFileName);
-
-#endif // CRASHHANDLER_H
