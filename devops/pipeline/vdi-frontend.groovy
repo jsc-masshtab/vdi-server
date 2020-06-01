d30b3f1d5d89d60c8b42e5054614fe1b7a95e603
Denis Proskurin <d.proskurin@mashtab.org>
2020-06-01 14:25:58 +0300

    FEATURE TG-8511 смена репо на vdi-testing

--- devops/pipeline/vdi-frontend.groovy
+++ devops/pipeline/vdi-frontend.groovy
@@ -17,8 +17,9 @@ properties([
     ),
     gitLabConnection('gitlab'),
     parameters([
-        string(      name: 'BRANCH',               defaultValue: 'feature_tg_8124',              description: 'branch', trim: false),
-        string(      name: 'REPO',                 defaultValue: 'vdi',              description: 'repo for uploading', trim: false),
+        string(      name: 'BRANCH',               defaultValue: 'master',           description: 'branch', trim: false),
+        // string(      name: 'REPO',                 defaultValue: 'vdi',              description: 'repo for uploading', trim: false),
+        string(      name: 'REPO',                 defaultValue: 'vdi-testing',      description: 'repo for uploading', trim: false),
         string(      name: 'VERSION',              defaultValue: '2.0.0',            description: 'base version',  trim: false),
         string(      name: 'AGENT',                defaultValue: 'debian9',          description: 'jenkins agent label for running the job', trim: false),
     ])
