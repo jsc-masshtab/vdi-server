9d3119e8f8997b3e1257839cf717dd2190c50d96
Solomin <a.solomin@mashtab.org>
2020-07-03 13:54:36 +0300

    FEATURE_TG_8649 Теперь stop_backend.sh точно остановит vdi

--- backend/stop_backend.sh
+++ backend/stop_backend.sh
@@ -1,2 +1,10 @@
-#!/bin/bash 
-killall -s SIGTERM python
+#!/bin/bash
+# по-хорошему
+killall -s SIGTERM python  > /dev/null 2>&1
+killall -s SIGTERM python3  > /dev/null 2>&1
+
+sleep 1
+
+# по-плохому
+killall -9 python  > /dev/null 2>&1
+killall -9 python3  > /dev/null 2>&1
