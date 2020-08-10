da1389c6d48c8e632d923ad7613e71e5cfb47f4c
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2020-08-10 12:56:25 +0300

    FEATURE TG-8091: veil-api cache
    подключил кэш

--- backend/common/veil/veil_api.py
+++ backend/common/veil/veil_api.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """Обобщенный функционал напрямую связанный с VeiL ECP."""
-from veil_api_client import VeilClientSingleton
+from veil_api_client import VeilClientSingleton, VeilCacheOptions
 
-from common.settings import VEIL_REQUEST_TIMEOUT
+from common.settings import VEIL_REQUEST_TIMEOUT, VEIL_CACHE_TTL, VEIL_CACHE_TYPE, VEIL_CACHE_SERVER
 
 
-veil_client = VeilClientSingleton(timeout=VEIL_REQUEST_TIMEOUT)
+cache_options = VeilCacheOptions(ttl=VEIL_CACHE_TTL, cache_type=VEIL_CACHE_TYPE, server=VEIL_CACHE_SERVER)
+veil_client = VeilClientSingleton(timeout=VEIL_REQUEST_TIMEOUT, cache_opts=cache_options)
 
 
 def get_veil_client() -> 'VeilClientSingleton':
