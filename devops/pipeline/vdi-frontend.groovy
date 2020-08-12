f4a270d0f1eef8c9a00561863e9a8952a8d34b2e
Alexey Gogolev <a.gogolev@mashtab.org>
2020-08-12 17:23:28 +0300

    FEATURE TG-9339: create local settings updates
    добавил дублирование значений по умолчанию для партиций и журнала

--- devops/pipeline/vdi-frontend.groovy
+++ devops/pipeline/vdi-frontend.groovy
@@ -1,178 +1,155 @@
 def currentDate = new Date().format('yyyyMMddHHmmss')
-def slackNotify = false
+def rocketNotify = true
 
-// git@192.168.10.145:vdi/vdi-server.git
-// $BRANCH
-// devops/pipeline/vdi-frontend.groovy
+notifyBuild(rocketNotify, ":bell: STARTED", "Start new build. Version: ${currentDate}")
 
-// build properties
-properties([
-    buildDiscarder(
-        logRotator(
-            artifactDaysToKeepStr: '',
-            artifactNumToKeepStr: '',
-            daysToKeepStr: '60',
-            numToKeepStr: '100'
-        )
-    ),
-    gitLabConnection('gitlab'),
-    parameters([
-        string(      name: 'BRANCH',               defaultValue: 'dev',           description: 'branch', trim: false),
-        // string(      name: 'REPO',                 defaultValue: 'vdi',              description: 'repo for uploading', trim: false),
-        string(      name: 'REPO',                 defaultValue: 'vdi-testing',      description: 'repo for uploading', trim: false),
-        string(      name: 'VERSION',              defaultValue: '2.0.0',            description: 'base version',  trim: false),
-        string(      name: 'AGENT',                defaultValue: 'debian9',          description: 'jenkins agent label for running the job', trim: false),
-    ])
-])
+pipeline {
+    agent {
+        label "${AGENT}"
+    }
 
-node("$AGENT") {
+    environment {
+        PRJNAME = "vdi-frontend"
+        NFS_DIR = "/nfs/vdi-deb"
+        DEB_ROOT = "${WORKSPACE}/devops/deb"
+        DATE = "${currentDate}"
+    }
 
-    env.PRJNAME="vdi-frontend"
-    env.NFS_DIR="vdi-deb"
-    env.DEB_ROOT="${WORKSPACE}/devops/deb"
-    env.DATE="$currentDate"
+    post {
+        failure {
+            println "Something goes wrong"
+            println "Current build marked as ${currentBuild.result}"
+            notifyBuild(rocketNotify,":x: FAILED", "Something goes wrong. Version: ${currentDate}")
+        }
 
+        aborted {
+            println "Build was interrupted manually"
+            println "Current build marked as ${currentBuild.result}"
+            notifyBuild(rocketNotify,":x: FAILED", "Build was interrupted manually. Version: ${currentDate}")
+        }
 
-    notifyBuild(slackNotify, "STARTED", "Start new build. Version: ${DATE}")
-    
-    timestamps {
-        ansiColor('xterm') {
-            try {
-                stage ('create environment') {
-                    cleanWs()
-                    def scmVars = checkout([ $class: 'GitSCM',
-                        branches: [[name: '$BRANCH']],
-                        doGenerateSubmoduleConfigurations: false,
-                        extensions: [], submoduleCfg: [],
-                        userRemoteConfigs: [[credentialsId: '', url: 'git@gitlab.bazalt.team:vdi/vdi-server.git']]
-                    ])
-                    
-                    println "env.DATE - $env.DATE"
-                }
+        success {
+            notifyBuild(rocketNotify, ":white_check_mark: SUCCESSFUL","Build SUCCESSFUL. Version: ${currentDate}")
+        }
+    }
 
-                stage ('prepare environment') {
-                    
+    options {
+        skipDefaultCheckout(true)
+        buildDiscarder(logRotator(daysToKeepStr: '60', numToKeepStr: '100'))
+        gitLabConnection('gitlab')
+        timestamps()
+        ansiColor('xterm')
+        parallelsAlwaysFailFast()
+    }
 
-                    sh script: '''
-                        rm -rf ${WORKSPACE}/.git ${WORKSPACE}/.gitignore
+    parameters {
+        string(      name: 'BRANCH',               defaultValue: 'dev',              description: 'branch')
+        string(      name: 'REPO',                 defaultValue: 'vdi-testing',      description: 'repo for uploading')
+        string(      name: 'VERSION',              defaultValue: '2.1.0',            description: 'base version')
+        string(      name: 'AGENT',                defaultValue: 'debian9',          description: 'jenkins agent label for running the job')
+    }
 
-                        echo "Install base packages"
-
-                        sudo sed -i s/us\\./ru\\./g /etc/apt/sources.list
-                        sudo apt-get update -y
-
-                        sudo apt-get install -y postgresql-server-dev-9.6 python3-dev python3-setuptools python-dev gcc python3-pip postgresql htop mc nginx libsasl2-dev libldap2-dev libssl-dev sudo curl apt-utils
-
-                        echo "Installing node v.10 && npm"
-                        curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
-                        sudo apt-get install -y nodejs
-
-                        # version software
-                        VER="${VERSION}-${BUILD_NUMBER}"
-                        sed -i "s:%%VER%%:$VER:g" "$DEB_ROOT/$PRJNAME/root/DEBIAN/control"
-
-                    '''
-                }
-
-                stage ('build') {
-                    sh script: '''
-                        cd ${WORKSPACE}/frontend
-                        npm install --unsafe-perm
-                        npm run build -- --prod
-
-                        # frontend compiled ${WORKSPACE}/frontend/dist/frontend
-
-                        mkdir -p "${DEB_ROOT}/${PRJNAME}/root/opt/veil-vdi/www"
-                        sudo mkdir -p /opt/veil-vdi/www
-                        cp -r ${WORKSPACE}/frontend/dist/frontend/* "${DEB_ROOT}/${PRJNAME}/root/opt/veil-vdi/www"
-                        sudo cp -r ${WORKSPACE}/frontend/dist/frontend/* /opt/veil-vdi/www
-
-                        make -C ${DEB_ROOT}/${PRJNAME}
-
-                    '''
-                }
-
-                stage ('publish to nfs') {
-                    sh script: '''
-                        # upload to nfs
-                        mkdir -p /nfs/${NFS_DIR}
-                        rm -f /nfs/vdi-deb/${PRJNAME}*.deb
-                        cp ${DEB_ROOT}/${PRJNAME}/*.deb /nfs/${NFS_DIR}/
-                    '''
-                }
-
-                stage ('publish to repo') {
-                    sh script: '''
-                        echo "REPO - $REPO"
-                     
-                        # upload files to temp repo
-                        DEB=$(ls -1 ${DEB_ROOT}/${PRJNAME}/*.deb)
-                        for ITEM in $DEB
-                        do
-                            echo "Processing packet: $ITEM"
-                            curl -sS -X POST -F file=@$ITEM http://qa2:8008/api/files/veil-${REPO}
-                            echo ""
-                        done
-
-                        # upload folder
-                        curl -sS -X POST http://qa2:8008/api/repos/veil-${REPO}/file/veil-${REPO}
-                        echo ""
-
-                        # make snapshot repo
-                        JSON1="{\\"Name\\":\\"veil-${REPO}-${DATE}\\"}"
-                        echo "JSON1 - $JSON1"
-                        curl -sS -X POST -H 'Content-Type: application/json' -d $JSON1 http://qa2:8008/api/repos/veil-${REPO}/snapshots
-                        echo ""
-
-                        # switch publish repo - aptly publish switch veil test veil-test-20180906161745
-                        JSON2="{\\"Snapshots\\":[{\\"Component\\":\\"main\\",\\"Name\\":\\"veil-${REPO}-${DATE}\\"}]}"
-                        echo "JSON2 - $JSON2"
-                        curl -sS -X PUT -H 'Content-Type: application/json' -d $JSON2 http://qa2:8008/api/publish/${REPO}/veil
-                        echo ""
-                    '''
-                }
-            } catch (InterruptedException err) {
-                    currentBuild.result = 'FAILURE'
-                    println "Build was interrupted manually"
-                    println "Current build marked as ${currentBuild.result}: ${err.toString()}"
-                    notifyBuild(slackNotify,"FAILED", "Build was interrupted manually. Version: ${DATE}")
-                    throw err
-            } catch (Exception err) {
-                    currentBuild.result = 'FAILURE'
-                    println "Something goes wrong"
-                    println "Current build marked as ${currentBuild.result}: ${err.toString()}"
-                    notifyBuild(slackNotify,"FAILED", "Something goes wrong. Version: ${DATE}")
-                    throw err
+    stages {
+        stage ('create environment') {
+            steps {
+                cleanWs()
+                checkout([ $class: 'GitSCM',
+                    branches: [[name: '$BRANCH']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [], submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId: '952e22ff-a42d-442c-83bd-76240a6ee793',
+                    url: 'git@gitlab.bazalt.team:vdi/vdi-server.git']]
+                ])
             }
         }
-    
-    notifyBuild(slackNotify, "SUCCESSFUL","Build SUCCESSFUL. Version: ${DATE}")
-    
+
+        stage ('prepare build environment') {
+            environment {
+                VER = "${VERSION}-${BUILD_NUMBER}"
+            }
+
+            steps {
+                sh script: '''
+                    rm -rf ${WORKSPACE}/.git ${WORKSPACE}/.gitignore
+                    sed -i "s:%%VER%%:$VER:g" "$DEB_ROOT/$PRJNAME/root/DEBIAN/control"
+                '''
+            }
+        }
+
+        stage ('build') {
+            steps {
+                sh script: '''
+                    # clean npm cache
+                    npm cache clean --force
+
+                    cd ${WORKSPACE}/frontend
+                    npm install --no-cache --unsafe-perm
+                    npm run build -- --prod
+
+                    # frontend compiled on ${WORKSPACE}/frontend/dist/frontend
+
+                    mkdir -p "${DEB_ROOT}/${PRJNAME}/root/opt/veil-vdi/www"
+                    cp -r ${WORKSPACE}/frontend/dist/frontend/* "${DEB_ROOT}/${PRJNAME}/root/opt/veil-vdi/www"
+
+                    make -C ${DEB_ROOT}/${PRJNAME}
+                '''
+            }
+        }
+
+        stage ('publish to nfs') {
+            steps {
+                sh script: '''
+                    # upload to nfs
+                    mkdir -p ${NFS_DIR}
+                    rm -f ${NFS_DIR}/${PRJNAME}*.deb
+                    cp ${DEB_ROOT}/${PRJNAME}/*.deb ${NFS_DIR}/
+                '''
+            }
+        }
+
+        stage ('publish to repo') {
+            steps {
+                sh script: '''
+                    echo "REPO - ${REPO}"
+
+                    # upload files to temp repo
+                    DEB=$(ls -1 ${DEB_ROOT}/${PRJNAME}/*.deb)
+                    for ITEM in $DEB
+                    do
+                        echo "Processing packet: $ITEM"
+                        curl -sS -X POST -F file=@$ITEM http://qa2:8008/api/files/veil-${REPO}
+                        echo ""
+                    done
+
+                    # upload folder
+                    curl -sS -X POST http://qa2:8008/api/repos/veil-${REPO}/file/veil-${REPO}
+                    echo ""
+
+                    # make snapshot repo
+                    JSON1="{\\"Name\\":\\"veil-${REPO}-${DATE}\\"}"
+                    echo "JSON1 - $JSON1"
+                    curl -sS -X POST -H 'Content-Type: application/json' -d $JSON1 http://qa2:8008/api/repos/veil-${REPO}/snapshots
+                    echo ""
+
+                    # switch publish repo - aptly publish switch veil test veil-test-20180906161745
+                    JSON2="{\\"Snapshots\\":[{\\"Component\\":\\"main\\",\\"Name\\":\\"veil-${REPO}-${DATE}\\"}]}"
+                    echo "JSON2 - $JSON2"
+                    curl -sS -X PUT -H 'Content-Type: application/json' -d $JSON2 http://qa2:8008/api/publish/${REPO}/veil
+                    echo ""
+                '''
+            }
+        }
     }
 }
 
-def notifyBuild(slackNotify, buildStatus, msg) {
+def notifyBuild(rocketNotify, buildStatus, msg) {
     buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    def summary = "${subject} (${env.BUILD_URL})" + "\n"
+    def summary = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})" + "\n"
 
-    if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-        summary += "${msg}"
-    } else if (buildStatus == 'SUCCESSFUL') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
-        summary += "${msg}"
-    } else {
-        color = 'RED'
-        colorCode = '#FF0000'
-        summary += "${msg}"
-    }
-    if (slackNotify){
-        slackSend (color: colorCode, message: summary)
+    summary += "${msg}"
+
+    if (rocketNotify){
+        rocketSend (channel: 'jenkins-notify', message: summary, serverUrl: '192.168.14.210', trustSSL: true, rawMessage: true)
     }
 }
