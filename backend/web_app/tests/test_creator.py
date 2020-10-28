bc3c98f083091bd1949c905b595a2a9451a17187
Solomin <a.solomin@mashtab.org>
2020-10-28 18:15:24 +0300

    FEATURE TG-10362 More than half tests work with virtual veil

--- backend/web_app/tests/test_creator.py
+++ backend/web_app/tests/test_creator.py
@@ -7,6 +7,7 @@ from web_app.tests.utils import execute_scheme
 
 from web_app.journal.schema import event_schema
 from common.models.auth import Group
+from common.models.event import Event
 
 pytestmark = [pytest.mark.asyncio, pytest.mark.creator]
 
@@ -18,17 +19,21 @@ async def test_event_creator(snapshot, fixt_db, fixt_auth_context):  # noqa
 
     await Group.soft_create('test', 'test_admin')
 
-    query = """{
-              events(limit: 5, offset: 0, event_type: 0, start_date: "%sZ", end_date: "%sZ", user: "test_admin") {
-                event_type
-                message,
-                description,
-                user
-              }
-            }""" % (fst.replace(microsecond=0).isoformat(), lst.replace(microsecond=0).isoformat())
+    try:
+        query = """{
+                  events(limit: 5, offset: 0, event_type: 0, start_date: "%sZ", end_date: "%sZ", user: "test_admin") {
+                    event_type
+                    message,
+                    description,
+                    user
+                  }
+                }""" % (fst.replace(microsecond=0).isoformat(), lst.replace(microsecond=0).isoformat())
 
-    executed = await execute_scheme(event_schema, query, context=fixt_auth_context)
-    snapshot.assert_match(executed)
-
-    group = await Group.get_object(include_inactive=True, extra_field_name='verbose_name', extra_field_value='test')
-    await group.delete()
+        executed = await execute_scheme(event_schema, query, context=fixt_auth_context)
+        snapshot.assert_match(executed)
+    except:  # noqa
+        raise
+    finally:
+        await Event.delete.where(Event.user == 'test_admin').gino.status()
+        group = await Group.get_object(include_inactive=True, extra_field_name='verbose_name', extra_field_value='test')
+        await group.delete()
