e7dbe6d6ff312f768ff00ac06636c820370c640a
Alexey Gogolev <a.gogolev@mashtab.org>
2021-02-02 12:14:59 +0300

    FEATURE TG-11603 veil-broker-iso

---
+++ devops/jenkins/veil-broker-iso.groovy
@@ -0,0 +1,125 @@
+def currentDate = new Date().format('yyyyMMddHHmmss')
+def rocketNotify = true
+
+notifyBuild(rocketNotify, ":bell: STARTED", "Start new build. Version: ${currentDate}")
+
+pipeline {
+    agent {
+        label "${AGENT}"
+    }
+    
+    environment {
+        APT_SRV = "192.168.11.118"
+        DATE = "${currentDate}"
+        ISO_NAME = "${REPO}-${VERSION}-${DATE}-${BUILD_NUMBER}"
+    }
+
+    post {
+        failure {
+            println "Something goes wrong"
+            println "Current build marked as ${currentBuild.result}"
+            notifyBuild(rocketNotify,":x: FAILED", "Something goes wrong. Version: ${currentDate}")
+        }
+
+        aborted {
+            println "Build was interrupted manually"
+            println "Current build marked as ${currentBuild.result}"
+            notifyBuild(rocketNotify,":x: FAILED", "Build was interrupted manually. Version: ${currentDate}")
+        }
+
+        success {
+            notifyBuild(rocketNotify, ":white_check_mark: SUCCESSFUL","Build SUCCESSFUL. Version: ${currentDate}")
+        }
+    }
+
+    options {
+        skipDefaultCheckout(true)
+        buildDiscarder(logRotator(daysToKeepStr: '60', numToKeepStr: '100'))
+        gitLabConnection('gitlab')
+        timestamps()
+        ansiColor('xterm')
+        parallelsAlwaysFailFast()
+    }
+
+    parameters {
+        string(      name: 'BRANCH',               defaultValue: 'dev',                   description: 'branch')
+        string(      name: 'REPO',                 defaultValue: 'veil-broker-test',      description: 'repo for uploading')
+        string(      name: 'VERSION',              defaultValue: '2.2.1',                 description: 'base version')
+        string(      name: 'AGENT',                defaultValue: 'bld-agent-02',          description: 'jenkins build agent')
+    }
+
+    stages {
+        stage ('checkout') {
+            steps {
+                cleanWs()
+                checkout([ $class: 'GitSCM',
+                    branches: [[name: '$BRANCH']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [], submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId: '952e22ff-a42d-442c-83bd-76240a6ee793',
+                    url: 'git@gitlab.bazalt.team:vdi/vdi-server.git']]
+                ])
+            }
+        }
+
+        stage('prepare build image') {
+            steps {
+                sh "docker build -f devops/docker/Dockerfile.vdi . -t vdi-builder:${VERSION}"
+            }
+        }
+
+        stage ('build') {
+            agent {
+                docker {
+                    image "vdi-builder:${VERSION}"
+                    args '-u root:root -v /nfs:/nfs'
+                    reuseNode true
+                    label "${AGENT}"
+                }
+            }
+
+            steps {
+                sh script: '''
+                    # download apt repo
+                    mkdir iso
+                    wget -r -nH -nv --reject="index.html*" http://${APT_SRV}/${REPO}/
+                    mv ${REPO}/ iso/repo
+
+                    # copy files
+                    cp -r devops/ansible/postgresql_cluster_2 iso/ansible
+                    rm -f iso/ansible/*.png iso/ansible/*.md iso/ansible/LICENSE
+                    cp devops/installer/install.sh iso
+
+                    # build iso
+                    genisoimage -o ./${ISO_NAME}.iso -V ${REPO} -R -J ./iso
+
+                    # copy iso to nfs
+                    mkdir -p /nfs/veil-broker-iso
+                    cp ${ISO_NAME}.iso /nfs/veil-broker-iso
+                '''
+            }
+        }
+
+        stage ('publish to repo') {
+            steps {
+                sh script: '''
+                    ssh uploader@192.168.10.144 mkdir -p /local_storage/veil-broker-iso
+                    scp /nfs/veil-broker-iso/${ISO_NAME}.iso uploader@192.168.10.144:/local_storage/veil-broker-iso
+                    rm -f /nfs/veil-broker-iso/${ISO_NAME}.iso
+                '''
+            }
+        }
+    }
+}
+
+def notifyBuild(rocketNotify, buildStatus, msg) {
+    buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+    def summary = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})" + "\n"
+
+    summary += "${msg}"
+
+    if (rocketNotify){
+        rocketSend (channel: 'jenkins-notify', message: summary, serverUrl: '192.168.14.210', trustSSL: true, rawMessage: true)
+    }
+}
