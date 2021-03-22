0434d6631293945373c80638c6eff5a64df94745
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2021-03-22 17:38:41 +0300

    FEATURE_TG_12562: micro version bump

--- backend/web_app/auth/handlers.py
+++ backend/web_app/auth/handlers.py
@@ -94,7 +94,7 @@ class LogoutHandler(BaseHandler, ABC):
 class VersionHandler(BaseHandler, ABC):
     async def get(self):
         info_dict = {
-            "version": "3.0.0",
+            "version": "3.0.1",
             "year": "2019-2021",
             "url": "https://mashtab.org",
             "copyright": "©mashtab.org",
