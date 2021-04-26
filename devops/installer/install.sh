7b4bd19e4fae8b6d5d391f0ef13b043acb8d914e
Alexey Gogolev <a.gogolev@mashtab.org>
2021-04-26 09:43:48 +0300

    FEATURE TG-12998 Trusted deb repo for iso installer

--- devops/installer/install.sh
+++ devops/installer/install.sh
@@ -11,5 +11,5 @@ apt-get install -y /media/cdrom/repo/pool/main/a/ansible/ansible_*.deb
 
 # run ansible-playbook
 cd /media/cdrom/ansible
-REPO="deb file:///media/cdrom/repo smolensk main"
+REPO="deb [trusted=yes] file:///media/cdrom/repo smolensk main"
 ansible-playbook main.yml --extra-vars "broker_apt_repo='$REPO'"
