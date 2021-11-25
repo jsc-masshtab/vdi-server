f92edcccbc6d2cd49ee1da718d6c067b44a9668a
Alexey Nevskiy <a.nevskiy@mashtab.org>
2021-11-25 19:34:12 +0300

    FEATURE TG-15023 изменены команды

--- backend/common/backup.py
+++ backend/common/backup.py
@@ -9,14 +9,12 @@ from common.veil.veil_errors import SilentError, SimpleError
 
 class Backup:
     @classmethod
-    def create_backup(cls, db_user="postgres", db_host="localhost", db_port="5432",
-                      backup_dir="/home/", backup_name="cluster.sql", creator="system"):
+    def create_backup(cls, backup_dir="/var/lib/postgresql/", backup_name="cluster.sql", creator="system"):
         """Создает бекап БД с помощью 'pg_dumpall'."""
         backup_file = cls.generate_name_with_timestamp(backup_name)
         backup_file = backup_dir + backup_file
 
-        command_pattern = "pg_dumpall -U {} -h {} -p {} --clean --if-exists --file={}"
-        backup_command = command_pattern.format(db_user, db_host, db_port, backup_file)
+        backup_command = "sudo -u postgres pg_dumpall --clean --if-exists --file={}".format(backup_file)
 
         result_backup = subprocess.run(shlex.split(backup_command))
 
@@ -39,11 +37,11 @@ class Backup:
     def terminate_sessions(db_name="vdi", db_user="postgres", creator="system"):
         """Отключает все активные соединения у БД."""
         forbid_connections = \
-            """psql -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = '{}';" -U {}"""
+            """sudo -u postgres psql -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = '{}';" -U {}"""
         forbid_connections = forbid_connections.format(db_name, db_user)
 
         terminate_connections = \
-            """psql -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{}';" -U {}"""
+            """sudo -u postgres psql -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{}';" -U {}"""
         terminate_connections = terminate_connections.format(db_name, db_user)
 
         result_forbid_connections = subprocess.run(shlex.split(forbid_connections), stdout=subprocess.DEVNULL)
@@ -65,11 +63,10 @@ class Backup:
         return result_terminate_sessions.returncode
 
     @classmethod
-    def restore_db(cls, db_user="postgres", backup_dir="/home/",
-                   backup_file="cluster.sql", creator="system"):
+    def restore_db(cls, backup_dir="/var/lib/postgresql/", backup_file="cluster.sql", creator="system"):
         """Восстанавливает БД из бекапа с помощью 'psql'."""
         backup_file = backup_dir + backup_file
-        restore_db = "psql -U {} -f {} postgres".format(db_user, backup_file)
+        restore_db = "sudo -u postgres psql -f {} postgres".format(backup_file)
 
         cls.terminate_sessions()
 
@@ -77,7 +74,7 @@ class Backup:
 
         if result_restore_db.returncode != 0:
             raise SimpleError(
-                _local_("Restore BD failed."),
+                _local_("Restore DB failed."),
                 description="Return code: {}".format(result_restore_db.returncode),
                 user=creator
             )
@@ -93,7 +90,7 @@ class Backup:
     @classmethod
     def drop_db(cls, db_name, creator="system"):
         """Удаляет БД."""
-        drop = """psql -c "DROP DATABASE {};" -U postgres""".format(db_name)
+        drop = """sudo -u postgres psql -c "DROP DATABASE {};" -U postgres""".format(db_name)
 
         cls.terminate_sessions()
 
