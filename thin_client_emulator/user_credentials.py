9f016a072eb6891acfef8165dd049f98d3863172
Aleksandr Solomin <a.solomin@mashtab.org>
2022-01-19 09:20:40 +0000

    Feature tg 15521

---
+++ thin_client_emulator/user_credentials.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+USER_CREDENTIALS = [
+    ("user1", "Bazalt1!"),
+    ("user2", "Bazalt1!"),
+    ("user3", "Bazalt1!"),
+    ("user4", "Bazalt1!"),
+    ("user5", "Bazalt1!")
+]
