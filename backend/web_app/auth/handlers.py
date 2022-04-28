866f14e10711c38540ac88a6a044b53eae53b96b
a.solomin <a.solomin@mashtab.org>
2022-04-28 15:05:02 +0300

    FIX TG-16540 Patch bump

--- backend/web_app/auth/handlers.py
+++ backend/web_app/auth/handlers.py
@@ -128,7 +128,7 @@ class LogoutHandler(BaseHttpHandler, ABC):
 class VersionHandler(BaseHttpHandler, ABC):
     async def get(self):
         info_dict = {
-            "version": "3.2.1",
+            "version": "3.2.2",
             "year": "2019-2022",
             "url": "https://mashtab.org",
             "copyright": "© mashtab.org",
