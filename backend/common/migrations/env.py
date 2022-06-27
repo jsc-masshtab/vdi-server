b38bff1f057c087f1e12e674d75aba8577b01931
i.dorovskikh <i.dorovskikh@mashtab.org>
2022-06-27 19:15:10 +0300

    Изменена структура разделов по установке Брокера

--- backend/common/migrations/env.py
+++ backend/common/migrations/env.py
@@ -30,7 +30,7 @@ fileConfig(config.config_file_name)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+# target_metadata = mymodel.base.metadata
 
 
 # other values from the config, defined by the needs of env.py,
