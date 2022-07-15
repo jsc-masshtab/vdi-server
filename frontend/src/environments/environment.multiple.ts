a350f6a801f4a7452e2806256a94b1d3c87dfbd5
Ruslan <r.belov@mashtab.org>
2022-07-15 15:13:05 +0300

    FEATURE TG-17429 Angular configuration for multibroker

--- frontend/src/environments/environment.multiple.ts
+++ frontend/src/environments/environment.multiple.ts
@@ -3,12 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   api: 'api/',
   api_ws: 'api/',
-  multiple: false
+  multiple: true
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
