3d4d472e2b6a3aa915c726ca8f8bc0548cd35ef4
Ruslan <r.belov@mashtab.org>
2022-08-17 14:53:45 +0300

    FEATURE TG-17710 X2GO canceled

--- frontend/src/app/pages/pools/add-pool/add-pool.component.ts
+++ frontend/src/app/pages/pools/add-pool/add-pool.component.ts
@@ -239,7 +239,7 @@ export class PoolAddComponent implements OnInit, OnDestroy {
 
   resetData() {
     this.data = {
-      connection_types: this.type === 'rds' ? ['RDP', 'NATIVE_RDP'] : ['RDP', 'NATIVE_RDP', 'SPICE', 'SPICE_DIRECT', 'X2GO', 'LOUDPLAY'],
+      connection_types: this.type === 'rds' ? ['RDP', 'NATIVE_RDP'] : ['RDP', 'NATIVE_RDP', 'SPICE', 'SPICE_DIRECT', 'LOUDPLAY'],
       controllers: [],
       resource_pools: [],
       data_pools: [],
